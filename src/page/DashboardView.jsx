2009e3779f0b41a9a117713d2abe0c2346530ff1
Jhonny Cave <jhonnycave@Jhonnys-MacBook-Pro.local>
2022-07-07 11:41:24 -0500

    Dashboard view : init

--- src/page/DashboardView.jsx
+++ src/page/DashboardView.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import AuthProvider from "../components/AuthProvider";
+
+function handleUserLoggedIn(user) {}
+
+function handleUserNotRegister(user) {}
+
+function handleUserNotLoggedIn() {}
 
 function DashboardView() {
-  return (
-    <div>DashboardView</div>
-  )
+    return (
+        <AuthProvider
+            onUserLoggedIn={handleUserLoggedIn}
+            onUserNotRegister={handleUserNotRegister}
+            onUserNotLoggedIn={handleUserNotLoggedIn}
+        >
+            <div>DashboardView</div>
+        </AuthProvider>
+    );
 }
 
-export default DashboardView
+export default DashboardView;
