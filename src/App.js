aa2b00636a14e4d1a845c044666c27f660fd17bc
jhonnycave <luis.j.cueva@gmail.com>
2022-07-08 16:52:15 -0500

    css login

--- src/App.js
+++ src/App.js
@@ -1,11 +1,127 @@
 import "./App.css";
+import * as React from "react";
+
+//MUI
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardHeader from "@mui/material/CardHeader";
+import CssBaseline from "@mui/material/CssBaseline";
+import Grid from "@mui/material/Grid";
+import StarIcon from "@mui/icons-material/StarBorder";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Link from "@mui/material/Link";
+import GlobalStyles from "@mui/material/GlobalStyles";
+import Container from "@mui/material/Container";
+
+// end MUI
+function Copyright(props) {
+    return (
+        <Typography
+            variant="body2"
+            color="text.secondary"
+            align="center"
+            {...props}
+        >
+            {"Copyright © "}
+            <Link color="inherit" href="https://youtube.com/"></Link>{" "}
+            {new Date().getFullYear()}
+            {"."}
+        </Typography>
+    );
+}
 
 function App() {
     return (
-        <div className="App">
-            <h1> Hola</h1>
-            <p>{process.env.REACT_APP_SALUDOS}</p>
-        </div>
+        <React.Fragment>
+            <GlobalStyles
+                styles={{ ul: { margin: 0, padding: 0, listStyle: "none" } }}
+            />
+            <CssBaseline />
+            <AppBar
+                position="static"
+                color="default"
+                elevation={0}
+                sx={{
+                    borderBottom: (theme) =>
+                        `1px solid ${theme.palette.divider}`,
+                }}
+            >
+                <Toolbar sx={{ flexWrap: "wrap" }}>
+                    <Typography
+                        variant="h6"
+                        color="inherit"
+                        noWrap
+                        sx={{ flexGrow: 1 }}
+                    ></Typography>
+                    <nav>
+                        <Link
+                            variant="button"
+                            color="text.primary"
+                            href="#"
+                            sx={{ my: 1, mx: 1.5 }}
+                        ></Link>
+                        <Link
+                            variant="button"
+                            color="text.primary"
+                            href="#"
+                            sx={{ my: 1, mx: 1.5 }}
+                        ></Link>
+                        <Link
+                            variant="button"
+                            color="text.primary"
+                            href="#"
+                            sx={{ my: 1, mx: 1.5 }}
+                        ></Link>
+                    </nav>
+                    <Link variant="button"  href="/login"> Login</Link>
+                </Toolbar>
+            </AppBar>
+            {/* Hero unit */}
+            <Container
+                disableGutters
+                maxWidth="sm"
+                component="main"
+                sx={{ pt: 8, pb: 6 }}
+            >
+                <Typography
+                    component="h1"
+                    variant="h2"
+                    align="center"
+                    color="text.primary"
+                    gutterBottom
+                >
+                    Inventario
+                </Typography>
+                <Typography
+                    variant="h5"
+                    align="center"
+                    color="text.secondary"
+                    component="p"
+                >
+                    demo de inventario
+                </Typography>
+            </Container>
+            {/* End hero unit */}
+
+            {/* Footer */}
+            <Container
+                maxWidth="md"
+                component="footer"
+                sx={{
+                    borderTop: (theme) => `1px solid ${theme.palette.divider}`,
+                    mt: 8,
+                    py: [3, 6],
+                }}
+            >
+                <Copyright sx={{ mt: 5 }} />
+            </Container>
+            {/* End footer */}
+        </React.Fragment>
     );
 }
 
--- src/page/LoginView.jsx
+++ src/page/LoginView.jsx
@@ -8,7 +8,35 @@ import { useNavigate } from "react-router-dom";
 import AuthProvider from "../components/AuthProvider";
 import { auth, userExistes } from "../firebase/firebase";
 
+// MUI
+
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+const theme = createTheme();
+
 function LoginView() {
+    //MUI
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+        console.log({
+            email: data.get("email"),
+            password: data.get("password"),
+        });
+    };
+
     /* const [currentUser, setCurrentUser] = useState(null); */
     const [state, setCurrentState] = useState(0);
     const navigate = useNavigate();
@@ -53,10 +81,45 @@ function LoginView() {
     } */
 
     if (state === 4) {
-        return <button onClick={handleOnClick}>Login with Google</button>;
+        return (
+            <ThemeProvider theme={theme}>
+                <Container component="main" maxWidth="xs">
+                    <CssBaseline />
+                    <Box
+                        sx={{
+                            marginTop: 8,
+                            display: "flex",
+                            flexDirection: "column",
+                            alignItems: "center",
+                        }}
+                    >
+                        <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+                            <LockOutlinedIcon />
+                        </Avatar>
+                        <Typography component="h1" variant="h5">
+                            Login
+                        </Typography>
+                        <Box
+                            component="form"
+                            onSubmit={handleSubmit}
+                            noValidate
+                            sx={{ mt: 1 }}
+                        >
+                            <Button
+                                type="submit"
+                                fullWidth
+                                variant="contained"
+                                sx={{ mt: 3, mb: 2 }}
+                                onClick={handleOnClick}
+                            >
+                                Login with Google
+                            </Button>
+                        </Box>
+                    </Box>
+                </Container>
+            </ThemeProvider>
+        );
     }
-
-   
 
     return (
         <AuthProvider
