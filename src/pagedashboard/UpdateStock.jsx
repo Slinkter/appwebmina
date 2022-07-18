c714329fe279f70654b09e364c1c14b0d8d583d8
jhonnycave <luis.j.cueva@gmail.com>
2022-07-18 11:42:07 -0500

    lunes laptop , actualizacion del css

--- src/pagedashboard/UpdateStock.jsx
+++ src/pagedashboard/UpdateStock.jsx
@@ -8,6 +8,8 @@ import * as Yup from "yup";
 import {
     Box,
     Button,
+    Card,
+    CardContent,
     Checkbox,
     Container,
     FormHelperText,
@@ -24,13 +26,13 @@ function UpdateStock() {
         initialValues: {
             nameproduct: "",
             currentAmout: "",
-            
+
             codigo: "",
         },
         validationSchema: Yup.object({
             nameproduct: Yup.string().max(150).required("campo faltante "),
             currentAmout: Yup.string().max(150).required("campo faltante "),
-           
+
             codigo: Yup.string().max(150).required("campo faltante"),
         }),
         onSubmit: (values) => {
@@ -46,91 +48,98 @@ function UpdateStock() {
                 component="main"
                 sx={{
                     alignItems: "center",
+                    justifyContent: "center",
                     display: "flex",
                     flexGrow: 1,
-                    minHeight: "100%",
                 }}
             >
-                <Container maxWidth="sm">
-                    <form onSubmit={formik.handleSubmit}>
-                        <Box sx={{ my: 3 }}>
-                            <Typography color="textPrimary" variant="h4">
-                                Actualizar inventario stock producto
-                            </Typography>
-                        </Box>
-                        <TextField
-                            error={Boolean(
-                                formik.touched.nameproduct &&
-                                    formik.errors.nameproduct
-                            )}
-                            fullWidth
-                            helperText={
-                                formik.touched.nameproduct &&
-                                formik.errors.nameproduct
-                            }
-                            label="Nombre Producto"
-                            margin="normal"
-                            name="nameproduct"
-                            onBlur={formik.handleBlur}
-                            onChange={formik.handleChange}
-                            value={formik.values.nameproduct}
-                            variant="outlined"
-                        />
-                        <TextField
-                            error={Boolean(
-                                formik.touched.detail && formik.errors.detail
-                            )}
-                            fullWidth
-                            helperText={
-                                formik.touched.detail && formik.errors.detail
-                            }
-                            label="Actual Cantidad"
-                            disabled
-                            margin="normal"
-                            name="currentAmout"
-                            onBlur={formik.handleBlur}
-                            onChange={formik.handleChange}
-                            type="text"
-                            value={formik.values.currentAmout}
-                            variant="outlined"
-                        />
+                <Card>
+                    <CardContent>
+                        <Container maxWidth="sm">
+                            <form onSubmit={formik.handleSubmit}>
+                                <Box sx={{ my: 3 }}>
+                                    <Typography
+                                        color="textPrimary"
+                                        variant="h4"
+                                    >
+                                        Actualizar inventario stock producto
+                                    </Typography>
+                                </Box>
+                                <TextField
+                                    error={Boolean(
+                                        formik.touched.nameproduct &&
+                                            formik.errors.nameproduct
+                                    )}
+                                    fullWidth
+                                    helperText={
+                                        formik.touched.nameproduct &&
+                                        formik.errors.nameproduct
+                                    }
+                                    label="Nombre Producto"
+                                    margin="normal"
+                                    name="nameproduct"
+                                    onBlur={formik.handleBlur}
+                                    onChange={formik.handleChange}
+                                    value={formik.values.nameproduct}
+                                    variant="outlined"
+                                />
+                                <TextField
+                                    error={Boolean(
+                                        formik.touched.detail &&
+                                            formik.errors.detail
+                                    )}
+                                    fullWidth
+                                    helperText={
+                                        formik.touched.detail &&
+                                        formik.errors.detail
+                                    }
+                                    label="Actual Cantidad"
+                                    disabled
+                                    margin="normal"
+                                    name="currentAmout"
+                                    onBlur={formik.handleBlur}
+                                    onChange={formik.handleChange}
+                                    type="text"
+                                    value={formik.values.currentAmout}
+                                    variant="outlined"
+                                />
 
-                  
+                                <TextField
+                                    error={Boolean(
+                                        formik.touched.lastName &&
+                                            formik.errors.lastName
+                                    )}
+                                    fullWidth
+                                    helperText={
+                                        formik.touched.lastName &&
+                                        formik.errors.lastName
+                                    }
+                                    label="Cantidad"
+                                    margin="normal"
+                                    name="codigo"
+                                    type="number"
+                                    onBlur={formik.handleBlur}
+                                    onChange={formik.handleChange}
+                                    value={formik.values.lastName}
+                                    variant="outlined"
+                                />
 
-                        <TextField
-                            error={Boolean(
-                                formik.touched.lastName &&
-                                    formik.errors.lastName
-                            )}
-                            fullWidth
-                            helperText={
-                                formik.touched.lastName &&
-                                formik.errors.lastName
-                            }
-                            label="Cantidad"
-                            margin="normal"
-                            name="codigo"
-                            type="number"
-                            onBlur={formik.handleBlur}
-                            onChange={formik.handleChange}
-                            value={formik.values.lastName}
-                            variant="outlined"
-                        />
-
-                        <Box sx={{ py: 2 }}>
-                            <Button
-                                color="primary"
-                                disabled={formik.isSubmitting}
-                                fullWidth
-                                size="large"
-                                type="submit"
-                                variant="contained"
-                            >
-                                Sign Up Now
-                            </Button>
-                        </Box>
-                    </form>
-                </Container>
+                                <Box sx={{ py: 2 }}>
+                                    <Button
+                                        color="primary"
+                                        disabled={formik.isSubmitting}
+                                        fullWidth
+                                        size="large"
+                                        type="submit"
+                                        variant="contained"
+                                    >
+                                        Sign Up Now
+                                    </Button>
+                                </Box>
+                            </form>
+                        </Container>
+                    </CardContent>
+                </Card>
             </Box>
         </DashboardWrapper>
     );
